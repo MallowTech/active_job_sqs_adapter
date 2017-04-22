3e389b88e8bd12cc0353b863a9cd8912c286606d
Gokul Murali <gokul@mallow-tech.com>
2017-04-22 11:24:22 +0530

    * updated code and added some comments

--- lib/active_job_sqs_adapter.rb
+++ lib/active_job_sqs_adapter.rb
@@ -2,9 +2,20 @@ require "active_job_sqs_adapter/version"
 require 'aws-sdk-core'
 require 'multi_json'
 
-# Push the jobs into sqs queue
+# Push the jobs into SQS queue
 module ActiveJobSqsAdapter
   class << self
+    # Initialize and return the SQS client
+    def sqs
+      Aws::SQS::Client.new
+    end
+
+    # Get existing SQS queue url or create a new one
+    def get_queue_url(job)
+      sqs.create_queue(queue_name: job.queue_name)[:queue_url]
+    end
+
+    # Enqueue the job to SQS queue
     def enqueue(job)
       sqs.send_message(
           queue_url: get_queue_url(job),
@@ -12,6 +23,7 @@ module ActiveJobSqsAdapter
       )
     end
 
+    # Enqueue the job to SQS queue with delay timestamp
     def enqueue_at(job, timestamp)
       delay = timestamp.to_i - Time.current.to_i
       sqs.send_message(
@@ -19,14 +31,6 @@ module ActiveJobSqsAdapter
           message_body: MultiJson.dump(job.serialize),
           delay_seconds: delay,
       )
-    end
-
-    def sqs
-      Aws::SQS::Client.new
-    end
-
-    def get_queue_url(job)
-      sqs.create_queue(queue_name: job.queue_name)[:queue_url]
     end
   end
 
